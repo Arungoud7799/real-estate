aef4e6f673a46904a5920b2e0a6ac1256beda92b
Arun <thallapallyarun49@gmail.com>
2023-11-21 22:20:36 +0530

    connect to mongodb

--- mern-realEstate/api/server.js
+++ mern-realEstate/api/server.js
@@ -1,6 +1,18 @@
 import express from "express";
+import { mongoose } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const PORT = 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to MONGODB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
